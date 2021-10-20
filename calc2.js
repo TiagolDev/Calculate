7a45311770448cb48cce2be8785aba3ca32d9f8b
TiagolDev <oliveira.tsilva@hotmail.com>
2021-10-20 00:16:51 -0300

    Indexiei e encurtei os códigos dos aquivos html, css e js

--- calc2.js
+++ calc2.js
@@ -1,60 +1,24 @@
-
-
-function Calculate(oper){
-
-  
-    let num0 = Number(document.calcSimple.num0.value);
-    let num00 = Number(document.calcSimple.num00.value);
-    
-   if (oper == "addition") {
-         let res = parseInt(num0) + parseInt(num00);
-      
-   document.getElementById('res').innerHTML = res.toFixed([]).replace(".",",");
-    
-   } else if(oper == "subtration") {
-         let res = num0 - num00;
-   
-   document.getElementById('res').innerHTML = res.toFixed([]).replace(".",",");
-  
-   } else if (oper == "multiplication") {
-         let res = num0 * num00;
-         
-   document.getElementById('res').innerHTML = res.toFixed([]).replace(".",",");
-      
-   } else { (oper == "division") 
-         let res = num0 / num00;
-
-   document.getElementById('res').innerHTML = res.toFixed([2]).replace(".",",");
-     
-   }
-   }   
-
-
-    function Clean(){
-
-        let num0 = document.calcSimple.num0.value = " "
-        let num00 = document.calcSimple.num00.value = " "
-    
-    
-        if (oper == "addition") {
-            let res = parseInt(num0) + parseInt(num00);
-         } else {
-            if (oper == "subtration") {
-               let res = num0 - num00;
-            } else {
-               if (oper == "multiplication") {
-                  let res = num0 * num00;
-               } else {
-                  let res = num0 / num00;
-               }
-            }
+function Calculate(oper) {
+      let num0 = Number(document.calcSimple.num0.value)
+      let num00 = Number(document.calcSimple.num00.value)    
+         if (oper == "addition"){
+               let res = parseInt(num0) + parseInt(num00);
+            document.getElementById('res').innerHTML = res.toFixed([]).replace(".",",")
+         } else if (oper == "subtration"){
+               let res = num0 - num00     
+            document.getElementById('res').innerHTML = res.toFixed([]).replace(".",",")
+         } else if (oper == "multiplication"){
+               let res = num0 * num00;
+            document.getElementById('res').innerHTML = res.toFixed([]).replace(".",",")
+         } else { (oper == "division") 
+               let res = parseFloat(num0) / parseFloat(num00)
+            document.getElementById('res').innerHTML = res.toFixed([2]).replace(".", ",") //.toFixed([2]).replace
          }
-          
-        
-       let res = (num0) - (num00)
-
-       
-       document.getElementById('res').innerHTML = " ";
-
-     }
+      }
+function Clean() {
+   let num0 = document.calcSimple.num0.value = " "
+   let num00 = document.calcSimple.num00.value = " "    
+   let res = (num0) - (num00)
+   document.getElementById('res').innerHTML = " "
+}
    
